78547f50405f4d6bd301a7eba24763fc63787f70
Wojtek <wojciech.brzuszkiewicz@hotmail.com>
2021-01-28 22:12:40 +0100

    lcd

--- index.js
+++ index.js
@@ -69,3 +69,24 @@ wss.on('connection', (ws) => {
     });
   });
 });
+
+
+const Lcd = require('lcd');
+const lcd = new Lcd({rs: 22, e: 18, data: [16, 11, 12, 15], cols: 16, rows: 2});
+ 
+lcd.on('ready', _ => {
+  setInterval(_ => {
+    lcd.setCursor(0, 0);
+    lcd.print(new Date().toISOString().substring(11, 19), err => {
+      if (err) {
+        throw err;
+      }
+    });
+  }, 1000);
+});
+ 
+// If ctrl+c is hit, free resources and exit.
+process.on('SIGINT', _ => {
+  lcd.close();
+  process.exit();
+});
